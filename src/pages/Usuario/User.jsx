0288ad850905bed1caa9d0030b2fc8a98468cdf2
Tatiana Quesquen <tatianacarvallo3@gmail.com>
2023-11-07 22:18:52 -0500

    VisualizaTitler

--- src/pages/Usuario/User.jsx
+++ src/pages/Usuario/User.jsx
@@ -6,17 +6,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 import { show_alerta } from "../../components/Alerta/Alertas";
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 import axios from "axios";
 import { Spinner } from "@material-tailwind/react";
-
+import Title from "../indicencias/components/Title";
 
 function User() {
-
   const [posts, setPosts] = useState([]);
 
-  const nombre = sessionStorage.getItem('rol');
+  const nombre = sessionStorage.getItem("rol");
   const navigate = useNavigate();
   const [sidebarToggle] = useOutletContext();
   const [loading, setIsloading] = useState(true);
@@ -26,7 +25,7 @@ function User() {
   }, []);
 
   const getUsuario = () => {
-    fetch('https://incidencias-fiisi.up.railway.app/api/usuario')
+    fetch("https://incidencias-fiisi.up.railway.app/api/usuario")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -36,8 +35,7 @@ function User() {
       .catch((err) => {
         console.log(err.message);
       });
-  }
-
+  };
 
   const dataHeader = [
     {
@@ -68,59 +66,53 @@ function User() {
   const handleDelete = (id, title) => {
     const MySwal = withReactContent(Swal);
     MySwal.fire({
-      title: ' Estas seguro que eliminar al usuario ' + title + '?',
-      icon: 'question', text: 'confirma para eliminar al usuario',
-      showCancelButton: true, confirmButtonText: 'si, eliminar', cancelButoonText: 'cancelar'
+      title: " Estas seguro que eliminar al usuario " + title + "?",
+      icon: "question",
+      text: "confirma para eliminar al usuario",
+      showCancelButton: true,
+      confirmButtonText: "si, eliminar",
+      cancelButoonText: "cancelar",
     }).then((result) => {
       if (result.isConfirmed) {
-
-        axios.delete(`https://incidencias-fiisi.up.railway.app/api/usuario/${id}`)
+        axios
+          .delete(`https://incidencias-fiisi.up.railway.app/api/usuario/${id}`)
           .then((response) => {
             var tipo = response.status ? "success" : "error";
             console.log("tipo", tipo);
             var msj = response.data;
-            show_alerta(msj, tipo)
-            if (tipo === 'success') {
+            show_alerta(msj, tipo);
+            if (tipo === "success") {
               getUsuario();
             }
           })
           .catch((error) => {
-            console.error('Error al obtener los datos de la API', error);
+            console.error("Error al obtener los datos de la API", error);
           });
-
-
       } else {
-        show_alerta('No fue eliminado', 'info')
+        show_alerta("No fue eliminado", "info");
       }
     });
-
 
     // <Spinner className="h-16 w-16 text-gray-900/50" />
   };
 
   return (
     <>
-      {nombre === 'Administrador' ? <Navigate to="/Usuario/User" /> : <Navigate to="/404" />}
+      {nombre === "Administrador" ? (
+        <Navigate to="/Usuario/User" />
+      ) : (
+        <Navigate to="/404" />
+      )}
       <main className="h-full">
-
-
-
         <Navbar toggle={sidebarToggle} />
-
 
         {/* Tabla */}
         <div className="mainCard">
-
-          <div className="border w-full border-gray-200 bg-white py-4 px-6 rounded-md">
-
-            <h1 className="text-center text-black text-xl">
-              Usuarios
-            </h1>
-
+          <Title text={"Usuarios registrados"}></Title>
+          <div className="border w-full border-gray-200 bg-white py-1  px-6 rounded-md">
             {/* Inicia boton añadir */}
             <button
-              onClick={() =>
-                navigate("/Usuario/DetailsUser")}
+              onClick={() => navigate("/Usuario/DetailsUser")}
               className="bg-cyan-600 border-blue-500 text-gray-100 px-3 py-2 mt-5 mb-4 rounded-lg shadow-lg text-sm flex gap-2 items-center"
             >
               <div>
@@ -128,15 +120,13 @@ function User() {
               </div>
               <span>Añadir usuario</span>
             </button>
-            
-           
+
             <UserTable
               loading={loading}
               dataHeader={dataHeader}
               data={posts}
               handleDelete={handleDelete}
             />
-          
           </div>
         </div>
       </main>
